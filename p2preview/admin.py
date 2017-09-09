02601cd618a9dfc207f577a827e5b806b3d05e9a
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-09-09 11:44:53 +0530

    Registered some initial models in admin.

--- p2preview/admin.py
+++ p2preview/admin.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Person, Student, Instrutor
 
 # Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'password', 'lastLogined', 'personType', 'token']
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'sId', 'rollNo', 'branch']
+
+class InstrutorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'iId', 'department', 'office', 'visitingHours']
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Instrutor, InstrutorAdmin)
