19f2bbd182a54da0e79fda563a5eafaef2b97ba6
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-09-09 12:52:04 +0530

    Created view for login.

--- p2preview/views.py
+++ p2preview/views.py
@@ -1,9 +1,48 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from p2preview.models import Person, Student, Instrutor
+
+import string
+import random
 
 # Create your views here.
 def index(request):
     return HttpResponse('Welcome to P2P Review System.')
+
+@csrf_exempt
+def login(request):
+    """
+    Check crendentials
+    """
+    if request.method == 'POST':
+        person = Person.objects.filter(email=request.POST['email'], password=request.POST['password'])
+        if (person.count() == 1):
+            char_set = string.ascii_uppercase + string.digits + string.ascii_lowercase
+            person.update(token=''.join(random.sample(char_set*10, 10)))
+            data = {
+                'success': 1,
+                'message': 'Successfully logged In'
+            }
+        elif (person.count() > 1):
+            data = {
+                'success': 0,
+                'message': 'Multiple accounts are associated with this email id. Please contact administrator'
+            }
+        elif (person.count() == 0):
+            person = Person.objects.filter(email=request.POST['email'])
+            if (person.count() == 1):
+                data = {
+                    'success': 0,
+                    'message': 'Hi, ' + person[0].name + ' your password is wrong. Please try again'
+                }
+            else:
+                data = {
+                    'success': 0,
+                    'message': 'Multiple accounts are associated with this email id. Please contact administrator'
+                }
+
+        return JsonResponse(data, safe=True)
