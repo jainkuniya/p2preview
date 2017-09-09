6b296b0959e69eb6a599c861b15218a68a2932f5
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-09-09 12:10:54 +0530

    Created separate urls.py for each app.

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,13 +3,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from p2preview import urls as p2preview_urls
+
 import p2preview.views
+
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', p2preview.views.index, name='index'),
+    url(r'^$', include(p2preview_urls.urlpatterns)),
     url(r'^admin/', include(admin.site.urls)),
 ]
