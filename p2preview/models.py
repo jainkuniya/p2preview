61b40f8764552e2d12154d63f1290ddfc8ba4b5b
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-12-08 14:50:55 +0530

    Remove max_lenght constrait from Person model.

--- p2preview/models.py
+++ p2preview/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     name = models.CharField(max_length=45, blank=False, default='')
-    email = models.EmailField(max_length=50, blank=False, default='', unique=True)
+    email = models.EmailField(blank=False, default='', unique=True)
     password = models.CharField(max_length=20, blank=False, default='')
     personType = models.IntegerField(blank=False)
     lastLogined = models.DateTimeField(auto_now_add=True)
