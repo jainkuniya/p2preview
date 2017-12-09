9fd324e01e7c8b16a14f975ae8414559c668542d
Vishal Sanghai <vcoolsanghai@gmail.com>
2017-12-09 21:39:44 +0530

    Added static urls path.

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls import include, url
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 admin.autodiscover()
 
 from p2preview import urls as p2preview_urls
 
 import p2preview.views
 
-
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 urlpatterns = [
     url(r'^', include(p2preview_urls.urlpatterns)),
     url(r'^admin/', include(admin.site.urls)),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
