3b0712218be90bfdd3881ca083e7d72e7f0bdb0f
Vishal Sanghai <vcoolsanghai@gmail.com>
2017-12-09 20:11:33 +0530

    Add templates dir path.

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'gettingstarted.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'debug': True,
