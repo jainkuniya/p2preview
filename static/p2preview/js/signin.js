c80cb6c771c9e9bf250df270a40deb6b79eb236d
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-08-20 16:32:06 +0530

    login page: Add alert with a instruction message.

--- static/p2preview/js/signin.js
+++ static/p2preview/js/signin.js
@@ -1,3 +1,10 @@
+alert(`Important Instructions:
+1. This is a sample app with functional bugs, and you have 45 minutes of time to uncover all the the bugs. 
+2. To understand the product: https://www.youtube.com/watch?v=JmY7h8w4wEo
+3. Time starts as soon as you login so make sure you keep all your required things for testing and noting down the bugs like screenshot taker, notepad, pen paper etc.
+4. Do NOT logout until you plan to, because you won’t be allowed to take the test again.
+5. After the test, share the bugs or observations you had with the application.`);
+
 $("#signInForm").submit(function () {
   $('#id_error').text('');
 
